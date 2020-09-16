08a492604c150820879fb0bd3259c66c8b7963b7
Allerter <45076212+Allerter@users.noreply.github.com>
2020-09-16 22:42:59 +0430

    updated string session's message format

--- setup/string_session.py
+++ setup/string_session.py
@@ -9,7 +9,8 @@ with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     print("Check your saved messages on Telegram and copy the STRING_SESSION value.")
     session_string = client.session.save()
     saved_messages_template = f"""
-<code>STRING_SESSION</code>: <code>{session_string}</code>
+STRING_SESSION:
+<code>{session_string}</code>
 ⚠️ <i>Please be carefull when passing this value to third parties</i>"""
     client.loop.run_until_complete(
         client.send_message("me", saved_messages_template, parse_mode="html")
