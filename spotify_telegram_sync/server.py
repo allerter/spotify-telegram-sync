397075fb009fb0b97ad16bdf78a8cb70b795aae1
Allerter <45076212+allerter@users.noreply.github.com>
2021-04-14 14:01:00 +0430

    overhauled server use fastapi instead of flask use uvicorn to run the server

--- spotify_telegram_sync/server.py
+++ spotify_telegram_sync/server.py
@@ -1,120 +1,39 @@
-from constants import (SERVER_PORT,
-                       USING_WEB_SERVER)
-# from constants import (TELETHON_API_ID,
-#                      TELETHON_API_HASH,
-#                      TELETHON_SESSION_WEB,
-#                      spotify,
-#                      TELEGRAM_CHANNEL)
-from get_song_file import download_track
-from database import database
-from waitress import serve
-# from telethon import TelegramClient
-# from telethon.sessions import StringSession
-from flask import Flask, request
-
-from threading import Thread
-from time import time
+import asyncio
 import logging
 
-# web server
-app = Flask(__name__)
+import uvicorn
+from bot import prepare_clients, update_playlist
+from constants import SERVER_PORT
+from fastapi import FastAPI
 
-playback = {'artist': '', 'title': '', 'time': time()}
-
-# telegram client
-# client = TelegramClient(StringSession(TELEGRAM_SESSION_TWO),
-#                        TELETHON_API_ID, TELETHON_API_HASH)
-
-# disable hachoir warnings
-logging.getLogger("hachoir").setLevel(logging.CRITICAL)
+app = FastAPI()
+logger = logging.getLogger("sts.server")
 
 
-def check_playlist():
-    # You could have the playlist checked by
-    # sending requests to the web server at example.com/check_playlist
-    # but that would require another Telegram string session.
-    return
-
-    # get id, url and isrc of non-local songs
-    spotify_songs = []
-    tracks = spotify.playlist_items(SPOTIFY_PLAYLIST_ID)
-    for item in tracks.items:
-        track = item.track
-        if track and not track.is_local and track.external_ids.get('isrc'):
-            spotify_songs.append((track.id,
-                                  track.external_urls['spotify'],
-                                  track.external_ids['isrc']))
-
-    # get songs from db
-    playlist = database('select')
-    difference = len(spotify_songs) - len(playlist)
-    client.start()
-    # songs were added to the playlist
-    if difference > 0:
-        upload_to_db_songs = []
-        playlist = [x[0] for x in playlist]
-        # specify new songs
-        to_be_added = list(set([x[0] for x in spotify_songs]) - set(playlist))
-        to_be_added = [x for x in spotify_songs if x[0] in to_be_added]
-
-        # download each song and send it to the Telegram channel
-        for song in to_be_added:
-            file = download_track(isrc=song[2], output='file')[0]
-            uploaded_file = client.loop.run_until_complete(
-                client.upload_file(file, part_size_kb=512)
-            )
-            msg = client.loop.run_until_complete(
-                client.send_file(TELEGRAM_CHANNEL, uploaded_file))
-            upload_to_db_songs.append((song[0], str(msg.id)))
-
-        # add new songs to database
-        database('insert', upload_to_db_songs)
-    # songs were removed from the playlist
-    elif difference < 0:
-        # specify deleted songs
-        spotify_ids = list(set([x[0] for x in playlist])
-                           - set([x[0] for x in spotify_songs]))
-
-        # get telegram ids of deleted songs and delete them
-        # telegram_ids = [x[1] for x in database('select', {'spotify_id': spotify_ids})]
-        telegram_ids = [x[1] for x in playlist if x[0] in spotify_ids]
-        client.loop.run_until_complete(
-            client.delete_messages(TELEGRAM_CHANNEL, telegram_ids))
-
-        # remove deleted songs from database
-        database('delete', {'spotify_id': spotify_ids})
-    client.disconnect()
+@app.get("/")
+async def read_root():
+    return "OK"
 
 
-@app.route('/check_playlist', methods=['GET'])
-def check_playlist_route():
-    thread = Thread(target=check_playlist)
-    thread.start()
+async def check_playlist():
+    try:
+        queue = asyncio.Queue()
+        await prepare_clients(queue, use_httpx=False)
+        clients = await queue.get()
+        await update_playlist(**clients)
+    except Exception as e:
+        logger.error(e)
+        raise e
+    finally:
+        await clients["telegram"].disconnect()
+        await clients["spotify"].close()
+
+
+@app.get("/update_playlist")
+async def update_playlist_path():
+    asyncio.ensure_future(check_playlist())
     return "I'll check"
 
 
-@app.route('/', methods=['GET'])
-def home_route():
-    return 'OK'
-
-
-@app.route('/local_playback', methods=['GET', 'POST'])
-def local_playback_route():
-    if request.method == 'POST':
-        artist = request.form.get('artist')
-        title = request.form.get('title')
-        now_time = time()
-        playback.update({'artist': artist,
-                         'title': title,
-                         'time': now_time})
-        return 'Set'
-    elif request.method == 'GET':
-        return playback
-
-
-def main():
-    serve(app, host="0.0.0.0", port=SERVER_PORT)
-
-
-if __name__ == '__main__' or USING_WEB_SERVER:
-    main()
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=SERVER_PORT)
