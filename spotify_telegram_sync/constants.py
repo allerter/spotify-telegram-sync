36a8ac53526382341c81c2ff1926f01bc254d7ec
Allerter <45076212+allerter@users.noreply.github.com>
2021-04-14 14:01:28 +0430

    updated constants

--- spotify_telegram_sync/constants.py
+++ spotify_telegram_sync/constants.py
@@ -1,34 +1,26 @@
 """Some constants e.g. the spotify token"""
 import os
-import tekore as tk
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-SPOTIFY_REFRESH_TOKEN = os.environ.get('SPOTIFY_REFRESH_TOKEN')
-SPOTIFY_PLAYLIST_ID = os.environ.get('SPOTIFY_PLAYLIST_ID')
-TELEGRAM_CHANNEL = os.environ.get('TELEGRAM_CHANNEL')
-DATABASE_URL = os.environ.get('DATABASE_URL')
-SERVER_PORT = int(os.environ.get('PORT', 5000))
-SERVER_ADDRESS = 'https://' + os.environ.get('APP_NAME') + '.herokuapp.com'
-TELETHON_API_ID = os.environ.get('TELETHON_API_ID')
-TELETHON_API_HASH = os.environ.get('TELETHON_API_HASH')
-TELETHON_SESSION_WEB = os.environ.get('TELETHON_SESSION_WEB')
-TELETHON_SESSION_WORKER = os.environ.get('TELETHON_SESSION_WORKER')
-DEEZER_ARL_TOKEN = os.environ.get('DEEZER_ARL_TOKEN')
-UPDATE_BIOS = True if os.environ.get('UPDAE_BIOS', 'true').lower() == 'true' else False
-CHECK_CHANNEL_DELETED = (True if os.environ.get('CHECK_CHANNEL_DELETED', 'true').lower()
-                        == 'true' else False)
-CHECK_LOCAL_PLAYBACK = (True if os.environ.get('CHECK_LOCAL_PLAYBACK', 'true').lower()
-                        == 'true' else False)
-USING_WEB_SERVER = (True if os.environ.get('USING_WEB_SERVER', 'true').lower()
-                    == 'true' else False)
-if USING_WEB_SERVER is False:
-    CHECK_LOCAL_PLAYBACK = False
+
+SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
+SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
+SPOTIFY_REFRESH_TOKEN = os.environ["SPOTIFY_REFRESH_TOKEN"]
+SPOTIFY_PLAYLIST_ID = os.environ["SPOTIFY_PLAYLIST_ID"]
+TELEGRAM_CHANNEL = os.environ["TELEGRAM_CHANNEL"]
+DATABASE_URL = os.environ["DATABASE_URL"]
+TELETHON_API_ID = os.environ["TELETHON_API_ID"]
+TELETHON_API_HASH = os.environ["TELETHON_API_HASH"]
+TELETHON_SESSION_STRING = os.environ["TELETHON_SESSION_STRING"]
+DEEZER_ARL_TOKEN = os.environ["DEEZER_ARL_TOKEN"]
+UPDATE_BIOS = (
+    True if os.environ.get("UPDATE_BIOS", "false").lower() == "true" else False
+)
+UPDATE_PLAYLIST = (
+    True if os.environ.get("UPDATE_PLAYLIST", "false").lower() == "true" else False
+)
+CHECK_TELEGRAM = (
+    True if os.environ.get("CHECK_TELEGRAM", "false").lower() == "true" else False
+)
+SERVER_PORT = int(os.environ.get("SERVER_PORT", 5000))
 
 # This used to be the photo of the pinned message if the channel has no pic.
-DEFAULT_PIC = os.path.join(os.getcwd(), '..', 'logo.png')
-
-# spotify client
-cred = tk.RefreshingCredentials(SPOTIFY_CLIENT_ID,
-                                SPOTIFY_CLIENT_SECRET)
-token = cred.refresh_user_token(SPOTIFY_REFRESH_TOKEN)
-spotify = tk.Spotify(token, sender=tk.RetryingSender())
+DEFAULT_PIC = os.path.join(os.getcwd(), "..", "logo.png")
